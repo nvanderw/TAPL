22885fdcce1a1e115286dc64afff7d0edb281d35
Nick Vanderweit <nick.vanderweit@gmail.com>
2013-10-21 12:46:26 -0600

    More parser tests

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import ClassyPrelude
 
 import Test.HUnit
-import TAPL.Test.Parser
+import qualified TAPL.Test.Parser as Parser
 
 main :: IO ()
-main = void $ runTestTT testIdent
+main = void $ runTestTT Parser.allTests
--- src/TAPL/Test/Parser.hs
+++ src/TAPL/Test/Parser.hs
@@ -1,4 +1,4 @@
-module TAPL.Test.Parser where
+module TAPL.Test.Parser (allTests) where
 
 import ClassyPrelude
 
@@ -7,8 +7,31 @@ import Test.HUnit
 import TAPL.Expr
 import TAPL.Parser
 
+parseExpr :: ByteString -> Either ParseError Expr
+parseExpr = parse expr "test"
+
 testIdent :: Test
-testIdent = TestCase $ do
-    case parse expr "test" ("x" :: ByteString) of
-        Left err -> assertFailure . show $ err
-        Right tree -> (EIdent "x") @=? tree
+testIdent = TestCase $ case parseExpr "x" of
+    Left err -> assertFailure . show $ err
+    Right tree -> (EIdent "x") @=? tree
+
+testLambda :: Test
+testLambda = TestCase $ case parseExpr "\\x.[x]" of
+    Left err -> assertFailure . show $ err
+    Right tree -> (ELambda "x" (EIdent "x")) @=? tree
+
+testApp1 :: Test
+testApp1 = TestCase $ case parseExpr "(x y)" of
+    Left err -> assertFailure . show $ err
+    Right tree -> (EApp (EIdent "x") (EIdent "y")) @=? tree
+
+testApp2 :: Test
+testApp2 = TestCase $ case parseExpr "(x)" of
+    Left _ -> return ()
+    Right _ -> assertFailure "Parsing should fail on unary application"
+
+appTests :: Test
+appTests = TestList [testApp1, testApp2]
+
+allTests :: Test
+allTests = TestList [testIdent, testLambda, appTests]
